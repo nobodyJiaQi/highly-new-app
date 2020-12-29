4e1be1b7a8104e3493c7b6ec47a8669eb80b7036
lijiaqi <980867543@qq.com>
2020-12-29 17:29:16 +0800

    start

---
+++ src/main/java/com/hnu/highlynewapp/HighlyNewAppApplication.java
@@ -0,0 +1,13 @@
+package com.hnu.highlynewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlyNewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighlyNewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnu/highlynewapp/HighlyNewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnu.highlynewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlyNewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
